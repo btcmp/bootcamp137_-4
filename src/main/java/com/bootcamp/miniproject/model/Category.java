e621e8235f74a303f124345d8d6d509049640090
PISCA PRASETYO A W <PISCA PRASETYO A W@10.8.108.8>
2018-03-21 11:55:12 +0700

    Model Category

---
+++ src/main/java/com/bootcamp/miniproject/model/Category.java
@@ -0,0 +1,77 @@
+package com.bootcamp.miniproject.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="mp_mst_category")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	@NotNull
+	private String name;
+	@Column(name="created_by")
+	private long createdBy;
+	@Column (name="created_on")
+	private Date createdOn;
+	@Column (name="modified_by")
+	private long modifiedBy;
+	@Column(name="modified_on")
+	private Date modifiedOn;
+	@NotNull
+	private Boolean active;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(long createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public long getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(long modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+	public void setModifiedOn(Date modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+	public Boolean getActive() {
+		return active;
+	}
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+	
+}
